8b3506e58760349537fae41ac9a06bd226a58651
Mitch Allen <mitch.c.allen@gmail.com>
2017-01-04 13:28:08 -0700

    checking in latest gen'd dist files

--- dist/aframe-maze-component.js
+++ dist/aframe-maze-component.js
@@ -216,6 +216,44 @@ module.exports.Component = {
 var cgFactory = _dereq_("@mitchallen/connection-grid-square"),
     baseGrid = _dereq_("@mitchallen/maze-generator-core");
 
+/**
+ * Maze Generator Core {@link https://www.npmjs.com/package/@mitchallen/maze-generator-core|npm documentation}
+ * @module maze-generator-core
+ */
+
+/**
+ * Square Maze Generator
+ * @module maze-generator-square
+ * @augments module:maze-generator-core
+ */
+
+/**
+ * 
+ * A module for generating square mazes
+ * @module maze-generator-square-factory
+ */
+
+/** 
+* Factory method that returns a square maze generator object.
+* It takes one spec parameter that must be an object with x and y values specifying the size of the maze.
+* If x and y size values are less than one (0) they will be normalized to 0.
+* You can call create multiple times to create multiple mazes.
+* @param {Object} options Named parameters for generating a square maze
+* @param {number} options.x Width of the maze
+* @param {number} options.y Height of the maze
+* @returns {module:maze-generator-square}
+* @example <caption>Creating a maze-generator-square</caption>
+* var mazeFactory = require("@mitchallen/maze-generator-square");
+* let xSize = 5;
+* let ySize = 6;
+* var maze = mazeFactory.create({ x: xSize, y: ySize });
+* @example <caption>Calling create mulitple times</caption>
+* var mazeFactory = require("@mitchallen/maze-generator-square");
+* var maze1 = mazeFactory.create( { x: 5, y: 10 } );
+* var maze2 = mazeFactory.create( { x: 7, y: 20 } );
+* maze1.generate();
+* maze2.generate();
+*/
 module.exports.create = function (spec) {
 
     spec = spec || {};
@@ -237,21 +275,59 @@ module.exports.create = function (spec) {
         grid: connections
     });
 
-    return Object.assign(connections, {
-        printBorder: function printBorder() {
-            var row = "";
-            var lim = _x * 2;
-            for (var i = 0; i < lim; i++) {
-                row += i === 0 ? " " : "_";
-            }
-            console.log(row);
-        },
+    return Object.assign(obj, {
+
+        /** Print board to console. Review this method to discover how to draw a maze.
+          * Drawing a square maze work like this:
+          * <ul>
+          * <li>Draw the top border</li>
+          * <li>For each cell:</li>
+                <ul>
+          *     <li>if this is the first cell in the row, draw the western wall</li>
+          *     <li>if the cell is NOT connected to it's eastern neighbor, draw the east wall</li>
+          *     <li>if the cell is NOT connected to it's southern neighbor, draw the south wall</li>
+          *     </ul>
+          * </ul>
+          * @function
+          * @instance
+          * @memberof module:maze-generator-square
+          * @example <caption>console output</caption>
+              MAZE: 20, 20
+        _______________________________________
+        |_  |    ___  |___   _   _|  ___   _  | |
+        | | | |___  | |   |_  |_____| |  _|  _| |
+        |  _| |_  | |___| | |  _____  |_  | |_  |
+        |_  |  ___|_  | | |  _|  _  |___| | |   |
+        | | |_|  _____| | |_|  _| | |  ___|___| |
+        | |_____|    ___|_  | |  _|___|     |  _|
+        |_____   _|_|  _  | | |    _|  _| |_|_  |
+        |  _____|  ___| |___| |_| |  _|  _|  ___|
+        | |   |  _|_   _______|  _| | |_  | |   |
+        |  _| | |   | |  ___    |  _| |  _| |_| |
+        |_  |___| |___|  _|  _| | |_  |_  |_  | |
+        | | |  ___  | | |   |___|_  |_  |_  |_  |
+        | | |___  | | | | | |  _____|  ___|_____|
+        |  _|   | | | | | | |_  | |  _  |  _   _|
+        |_  | |___| | | | |_|  _| | |  _| | |_  |
+        |  _|___  | |  _|_____|_  | |_____|  _| |
+        |_  |  ___| |_  |   |   |___   ___  |  _|
+        |  _|_|  ___| | | |___| |   |_|   | |_  |
+        | |  ___| |   | | |  _| | |_  | | |___| |
+        |___|_______|_____|_______|_____|_______|
+          */
         printBoard: function printBoard() {
             console.log("MAZE: %d, %d", _x, _y);
-            this.printBorder();
+            // print top north walls
+            var border = "";
+            var lim = _x * 2;
+            for (var i = 0; i < lim; i++) {
+                border += i === 0 ? " " : "_";
+            }
+            console.log(border);
+            // print maze east and south walls
             var dirMap = this.dirMap;
             for (var y = 0; y < _y; y++) {
-                var row = "|";
+                var row = "|"; // print west wall
                 for (var x = 0; x < _x; x++) {
                     row += this.connects(x, y, "S") ? " " : "_";
                     if (this.connects(x, y, "E")) {
@@ -733,24 +809,23 @@ module.exports.create = function (spec) {
             }
         },
         generate: function generate(spec) {
-            var aMask = null,
-                start = null;
-            if (spec) {
-                aMask = spec.mask;
-                start = spec.start;
-            }
+
+            spec = spec || {};
+
+            var aMask = spec.mask || [],
+                start = spec.start || {},
+                x = start.c || 0,
+                y = start.r || 0;
+
             this.fill(0);
+
             for (var key in aMask) {
                 var mask = aMask[key];
                 this.mask(mask.c, mask.r);
             }
-            var maxDepth = this.xSize * this.ySize,
-                x = 0,
-                y = 0;
-            if (start) {
-                x = start.c || 0;
-                y = start.r || 0;
-            }
+
+            var maxDepth = this.xSize * this.ySize;
+
             this.carveMaze(x, y, 0, maxDepth);
         }
     });
